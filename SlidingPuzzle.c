46e095cfbc6a52b7d36d5cdeafd77afe4287b004
Paula Perdomo <mariapaulaperdomo@Maria-Paulas-MacBook-Air.local>
2021-04-07 22:51:57 -0400

    Implemented the basis of the game. Character buffer and necessary functions

--- SlidingPuzzle.c
+++ SlidingPuzzle.c
@@ -1,6 +1,189 @@
 #include <stdio.h>
+#include <stdbool.h>
+	
+/* This files provides address values that exist in the system */
+#define BOARD                 "DE1-SoC"
+
+/* Memory */
+#define DDR_BASE              0x00000000
+#define DDR_END               0x3FFFFFFF
+#define A9_ONCHIP_BASE        0xFFFF0000
+#define A9_ONCHIP_END         0xFFFFFFFF
+#define SDRAM_BASE            0xC0000000
+#define SDRAM_END             0xC3FFFFFF
+#define FPGA_ONCHIP_BASE      0xC8000000
+#define FPGA_ONCHIP_END       0xC803FFFF
+#define FPGA_CHAR_BASE        0xC9000000
+#define FPGA_CHAR_END         0xC9001FFF
+
+/* Cyclone V FPGA devices */
+#define LEDR_BASE             0xFF200000
+#define HEX3_HEX0_BASE        0xFF200020
+#define HEX5_HEX4_BASE        0xFF200030
+#define SW_BASE               0xFF200040
+#define KEY_BASE              0xFF200050
+#define JP1_BASE              0xFF200060
+#define JP2_BASE              0xFF200070
+#define PS2_BASE              0xFF200100
+#define PS2_DUAL_BASE         0xFF200108
+#define JTAG_UART_BASE        0xFF201000
+#define JTAG_UART_2_BASE      0xFF201008
+#define IrDA_BASE             0xFF201020
+#define TIMER_BASE            0xFF202000
+#define AV_CONFIG_BASE        0xFF203000
+#define PIXEL_BUF_CTRL_BASE   0xFF203020
+#define CHAR_BUF_CTRL_BASE    0xFF203030
+#define AUDIO_BASE            0xFF203040
+#define VIDEO_IN_BASE         0xFF203060
+#define ADC_BASE              0xFF204000
+
+/* Cyclone V HPS devices */
+#define HPS_GPIO1_BASE        0xFF709000
+#define HPS_TIMER0_BASE       0xFFC08000
+#define HPS_TIMER1_BASE       0xFFC09000
+#define HPS_TIMER2_BASE       0xFFD00000
+#define HPS_TIMER3_BASE       0xFFD01000
+#define FPGA_BRIDGE           0xFFD0501C
+
+/* ARM A9 MPCORE devices */
+#define   PERIPH_BASE         0xFFFEC000    // base address of peripheral devices
+#define   MPCORE_PRIV_TIMER   0xFFFEC600    // PERIPH_BASE + 0x0600
+
+/* Interrupt controller (GIC) CPU interface(s) */
+#define MPCORE_GIC_CPUIF      0xFFFEC100    // PERIPH_BASE + 0x100
+#define ICCICR                0x00          // offset to CPU interface control reg
+#define ICCPMR                0x04          // offset to interrupt priority mask reg
+#define ICCIAR                0x0C          // offset to interrupt acknowledge reg
+#define ICCEOIR               0x10          // offset to end of interrupt reg
+/* Interrupt controller (GIC) distributor interface(s) */
+#define MPCORE_GIC_DIST       0xFFFED000    // PERIPH_BASE + 0x1000
+#define ICDDCR                0x00          // offset to distributor control reg
+#define ICDISER               0x100         // offset to interrupt set-enable regs
+#define ICDICER               0x180         // offset to interrupt clear-enable regs
+#define ICDIPTR               0x800         // offset to interrupt processor targets regs
+#define ICDICFR               0xC00         // offset to interrupt configuration regs
+
+/* VGA colors */
+#define WHITE 0xFFFF
+#define YELLOW 0xFFE0
+#define RED 0xF800
+#define GREEN 0x07E0
+#define BLUE 0x001F
+#define CYAN 0x07FF
+#define MAGENTA 0xF81F
+#define GREY 0xC618
+#define PINK 0xFC18
+#define ORANGE 0xFC00
+#define BLACK 0x0000
+	
+/* Screen size. */
+#define RESOLUTION_X 320
+#define RESOLUTION_Y 240
+#define CHARACTER_X 80
+#define CHARACTER_Y 60
+	
+/* Constants for animation */
+#define BOX_LEN 2
+#define NUM_BOXES 8
+
+#define FALSE 0
+#define TRUE 1
+	
+volatile int pixel_buffer_start; // global variable
+void clear_screen();
+void plot_pixel(int x, int y, short int line_colour);
+void plot_character(int x, int y, char ASCII_code);
+void draw_line(int x0, int y0, int x1, int y1, short int line_colour);
+void swap(int *xp, int *yp);
 
 int main(){
-	printf("Hello");
-    printf("Hi back");
+	
+	volatile int * pixel_ctrl_ptr = (int *)PIXEL_BUF_CTRL_BASE;
+	volatile int * char_ctrl_ptr = (int *)CHAR_BUF_CTRL_BASE;
+    /* Read location of the pixel buffer from the pixel buffer controller */
+    pixel_buffer_start = *pixel_ctrl_ptr;
+    clear_screen();
+	
+	//pixel_buffer_start = *char_ctrl_ptr;
+	//plot_character(2, 50, 'H');
+	
+	
 }
+
+void clear_screen() {
+	for(int x = 0; x < RESOLUTION_X; x++){
+		for(int y = 0; y < RESOLUTION_Y; y++){
+			plot_pixel(x,y, BLACK);
+		}		
+	}
+	for(int x = 0; x < CHARACTER_X; x++) {
+		for(int y = 0; y < CHARACTER_Y; y++) {
+			plot_character(x, y, ' ');
+		}
+	}
+}
+
+
+
+
+//provides template for the address of the pixel.
+void plot_pixel(int x, int y, short int line_colour) {
+    *(short int *)(pixel_buffer_start + (y << 10) + (x << 1)) = line_colour;
+}
+
+void plot_character(int x, int y, char ASCII_code) {
+	*(short int *)(pixel_buffer_start + (y << 7) + x) = ASCII_code;
+}
+
+void draw_line(int x0, int y0, int x1, int y1, short int line_colour) { //contains starting values of the line
+	
+	bool is_steep = abs(y1-y0) > abs(x1-x0);
+	if(is_steep) {
+		swap(&x0, &y0);
+		swap(&x1, &y1);
+	}
+	if(x0 > x1) {
+		swap(&x0, &x1);
+		swap(&y0, &y1);
+	}
+	
+	int deltax = x1 - x0;
+	int deltay = abs(y1 - y0);
+	int error = -(deltax/2);
+	
+	int y = y0;
+	int y_step;
+	
+	if(y0 < y1) {
+		y_step = 1;
+	}
+	else {
+		y_step = -1;
+	}
+	
+	for(int x = x0; x < x1; ++x) {
+		if(is_steep) {
+			plot_pixel(y, x, line_colour);
+		}
+		else {
+			plot_pixel(x, y, line_colour);
+		}
+		
+		error = error + deltay;
+		if(error >= 0) {
+			y = y + y_step;
+			error = error - deltax;
+		}
+	}
+}
+
+void swap(int *xp, int *yp) {
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+	
+
+	
+
